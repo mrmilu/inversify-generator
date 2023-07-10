4b99411c06e46141fa08e02dbdc41e6c0b8fefd5
Arnau Gómez <agomez@mrmilu.com>
2023-07-10 13:32:56 +0200

    refactor: remove variable

--- src/services/get_dependencies.ts
+++ src/services/get_dependencies.ts
@@ -5,26 +5,26 @@ import { Dependency } from "../types/dependency.js";
 export function getDependencies(args: Partial<Args>) {
   const dependencies: Dependency[] = [];
 
-  const project = new Project({
-    tsConfigFilePath: args.tsconfig,
-  });
-  
-  project.getSourceFiles().forEach((sourceFile) => {
-    sourceFile.getClasses().forEach((classDeclaration) => {
-      const hasDecorator = Boolean(classDeclaration.getDecorator("injectable"));
-      if (!hasDecorator) return;
+  new Project({ tsConfigFilePath: args.tsconfig })
+    .getSourceFiles()
+    .forEach((sourceFile) => {
+      sourceFile.getClasses().forEach((classDeclaration) => {
+        const hasDecorator = Boolean(
+          classDeclaration.getDecorator("injectable")
+        );
+        if (!hasDecorator) return;
 
-      const className = classDeclaration.getName();
-      if (!className) return;
+        const className = classDeclaration.getName();
+        if (!className) return;
 
-      const implement = classDeclaration.getImplements()[0];
-      dependencies.push({
-        path: sourceFile.getFilePath().replace(/^.*src/, "@/src"),
-        abstraction: implement?.getText() ?? className,
-        implementation: className,
+        const implement = classDeclaration.getImplements()[0];
+        dependencies.push({
+          path: sourceFile.getFilePath().replace(/^.*src/, "@/src"),
+          abstraction: implement?.getText() ?? className,
+          implementation: className,
+        });
       });
     });
-  });
 
   return dependencies;
 }
